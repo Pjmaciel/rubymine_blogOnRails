0ba7623626b1c3124dbb118aecba6684c08e0fdb
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-28 19:17:23 -0300

    feat: add Internationalization I18n

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module RubymineBlogOnRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'Brasilia'
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
