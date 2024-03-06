73221cc9a5d91e0fe202b0466178168bc060a332
Pjmaciel <pjmaciel_4@hotmail.com>
2024-03-06 11:18:32 -0300

    fix: change the validity assignment for models and not for controller as was being used

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
-  validates :author, presence: true
-  validates :body, presence: true
   def create
-    comment = Comment.create!(comments_params)
-
-    redirect_to comment.post, notice: 'O seu comentario foi enviado'
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to comment.post, notice: 'O seu comentário foi enviado com sucesso!'
+    else
+      flash[:alert] = comment.errors.full_messages.to_sentence
+      redirect_to comment.post
+    end
   end
 
   private
 
-  def comments_params
-    params.require(:comment).permit(:post_id, :author,:body)
+  def comment_params
+    params.require(:comment).permit(:post_id, :author, :body)
   end
 end
